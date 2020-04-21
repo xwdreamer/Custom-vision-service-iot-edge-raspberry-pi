319c054ababf6bfeb53def66415b4a0ef03ba6f2
xwdreamer <xwdreamer06@gmail.com>
2020-04-21 13:58:37 +0800

    master

--- modules/ImageClassifierService/amd64.Dockerfile
+++ modules/ImageClassifierService/amd64.Dockerfile
@@ -1,13 +1,12 @@
-FROM tensorflow/tensorflow:latest-py3
+﻿FROM python:3.7-slim
 
-RUN echo "BUILD MODULE: ImageClassifierService"
+RUN pip install -U pip
+RUN pip install numpy==1.17.3 tensorflow==2.0.0 flask pillow
 
-COPY /build/amd64-requirements.txt amd64-requirements.txt
-
-# Install Python packages
-RUN pip install -r amd64-requirements.txt
- 
-ADD app /app
+RUN mkdir app
+COPY ./app/app-amd64.py ./app/app.py
+COPY ./app/predict-amd64.py ./app/predict.py
+COPY ./app/labels.txt ./app/model.pb ./app/
 
 # Expose the port
 EXPOSE 80
@@ -16,4 +15,4 @@ EXPOSE 80
 WORKDIR /app
 
 # Run the flask server for the endpoints
-CMD python app.py
+CMD python -u app.py
--- modules/SenseHatDisplay/test/UnitTests.py
+++ modules/SenseHatDisplay/test/UnitTests.py
@@ -7,12 +7,19 @@ import app.MessageParser
 class UnitTests(unittest.TestCase):
     def test_HighestProbabilityTagMeetingThreshold(self):
         MessageParser = app.MessageParser.MessageParser()
-        message1=json.loads("[{\"Tag\": \"banana\",\"Probability\": 0.4}, {\"Tag\": \"apple\",\"Probability\": 0.3}]")
-        self.assertEqual(MessageParser.highestProbabilityTagMeetingThreshold(message1, 0.5), 'none')
-        message2=json.loads("[{\"Tag\": \"banana\",\"Probability\": 0.4}, {\"Tag\": \"apple\",\"Probability\": 0.5}]")
-        self.assertEqual(MessageParser.highestProbabilityTagMeetingThreshold(message2, 0.3), 'apple')
-        message3=json.loads("[{\"Probability\": 0.038001421838998795, \"Tag\": \"apple\"}, {\"Probability\": 0.38567957282066345, \"Tag\": \"banana\"}]")
-        self.assertEqual(MessageParser.highestProbabilityTagMeetingThreshold(message3, 0.3), 'banana')
+        message1 = json.loads(
+            "{\"iteration\": \"\",\"id\": \"\",\"predictions\": [{\"probability\": 0.3,\"tagName\": \"Apple\",\"tagId\": \"\",\"boundingBox\": null},{\"probability\": 0.4,\"tagName\": \"Banana\",\"tagId\": \"\",\"boundingBox\": null}],\"project\": \"\",\"created\": \"2019-12-10T04:37:49.657555\"}")
+        self.assertEqual(
+            MessageParser.highestProbabilityTagMeetingThreshold(message1, 0.5), 'none')
+        message2 = json.loads(
+            "{\"iteration\": \"\",\"id\": \"\",\"predictions\": [{\"probability\": 0.5,\"tagName\": \"Apple\",\"tagId\": \"\",\"boundingBox\": null},{\"probability\": 0.4,\"tagName\": \"Banana\",\"tagId\": \"\",\"boundingBox\": null}],\"project\": \"\",\"created\": \"2019-12-10T04:37:49.657555\"}")
+        self.assertEqual(MessageParser.highestProbabilityTagMeetingThreshold(
+            message2, 0.3), 'Apple')
+        message3 = json.loads(
+            "{\"iteration\": \"\",\"id\": \"\",\"predictions\": [{\"probability\": 0.038001421838998795,\"tagName\": \"Apple\",\"tagId\": \"\",\"boundingBox\": null},{\"probability\": 0.38567957282066345,\"tagName\": \"Banana\",\"tagId\": \"\",\"boundingBox\": null}],\"project\": \"\",\"created\": \"2019-12-10T04:37:49.657555\"}")
+        self.assertEqual(MessageParser.highestProbabilityTagMeetingThreshold(
+            message3, 0.3), 'Banana')
+
 
 if __name__ == '__main__':
     unittest.main()
